d5663dc1174ca33f46259c65d380020dd40ac3f4
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-07-02 17:59:40 -0300

    Add install commands for a few essential apps

---
+++ first-boot-installs.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository ppa:git-core/ppa
+
+sudo apt-get install -y \
+	git fortune cowsay terminator byobu screenfetch xkbset lolcat xclip
+
+sudo snap install --classic code
+sudo snap install --classic shotcut
+sudo snap install \
+code chromium vlc spotify gimp htop postman youtube-dl plexmediaserver
+
+
+# set mouse speed when using the Universal Access Mouse Keys feature
+xkbset ma 60 10 10 20 10

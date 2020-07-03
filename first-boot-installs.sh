e114a35ddf43e32744e61c906da137417770419f
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-07-03 19:57:09 -0300

    Update first installations with more programs

--- first-boot-installs.sh
+++ first-boot-installs.sh
@@ -1,40 +1,39 @@
-sudo add-apt-repository ppa:git-core/ppa
+mkdir ~/apps
+
+sudo add-apt-repository ppa:git-core/ppa  # latest git
+sudo apt-add-repository -y ppa:cappelikan/ppa  # mainline (ukuu fork)
 
 sudo apt-get install -y \
+	bmon \
 	byobu \
 	cowsay \
 	fortune \
 	git git-annex \
+	hollywood \
 	lolcat \
+	mainline \
+	numlockx \
 	screenfetch \
 	terminator \
 	xclip xkbset
 
 sudo snap install --classic code
 sudo snap install --classic shotcut
+sudo snap install --classic sublime-text
 sudo snap install \
 code chromium vlc spotify gimp htop postman youtube-dl plexmediaserver
 
 
-# set mouse speed when using the Universal Access Mouse Keys feature
-xkbset ma 60 10 10 20 10
-
 # Change GtkIMContextSimple (emoji input) from "['<Control><Shift>e']"
-
 # so it won't interfere with Terminator's vertical split shortcut
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "['<Control><Shift>j']"
 
 # install nautilus-copypath extension to add "copy path" to the context menu of a file
 sudo apt-get install python-nautilus python3-gi  # installing gi here avoids a pip install PyGObject; right now, we don't have pip
-mkdir ~/apps
-pushd !$
+pushd ~/apps
 git clone https://github.com/ronen25/nautilus-copypath nautilus-copypath
-pushd !$
-mkdir ~/.local/share/nautilus-python
-mkdir ~/.local/share/nautilus-python/extensions
-cp nautilus-copypath.py ~/.local/share/nautilus-python/extensions/
-cp nautilus-copywinpath.py ~/.local/share/nautilus-python/extensions/
-popd
+mkdir -p ~/.local/share/nautilus-python/extensions
+cp ./nautilus-copypath/nautilus-copypath.py ~/.local/share/nautilus-python/extensions/
 popd
 
 # restart nautilus for py-extensions to reload
@@ -47,3 +46,5 @@ rm ~/anaconda.sh
 ~/miniconda/bin/conda init
 bash
 conda --version
+
+pip install gpustat

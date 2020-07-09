1a25d898d9d362f3f3992819cbbab65b6edf6605
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-07-09 16:09:57 -0300

    Add .bashrc and .inputrc dotfiles

--- create-dotfiles-symlinks.sh
+++ create-dotfiles-symlinks.sh
@@ -1,7 +1,9 @@
 dotfiles_folder=$(realpath ./dotfiles)
 
-ln -nfs "${dotfiles_folder}/.gitconfig" ~/.gitconfig;
 ln -nfs "${dotfiles_folder}/.bash_aliases" ~/.bash_aliases;
+ln -nfs "${dotfiles_folder}/.bashrc" ~/.bashrc;
+ln -nfs "${dotfiles_folder}/.gitconfig" ~/.gitconfig;
+ln -nfs "${dotfiles_folder}/.inputrc" ~/.inputrc;
 
 source ~/.bashrc;
 

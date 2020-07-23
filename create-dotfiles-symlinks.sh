f8cd931099d663e983781b2a3429ae070508a18b
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-07-23 00:34:41 -0300

    Add VS Code settings.json

--- create-dotfiles-symlinks.sh
+++ create-dotfiles-symlinks.sh
@@ -1,5 +1,8 @@
 dotfiles_folder=$(realpath ./dotfiles)
 
+mkdir -p ~/.config/Code/User/
+
+ln -nfs "${dotfiles_folder}/.config_-_Code_-_User_-_settings.json" ~/.config/Code/User/settings.json
 ln -nfs "${dotfiles_folder}/.bash_aliases" ~/.bash_aliases;
 ln -nfs "${dotfiles_folder}/.bashrc" ~/.bashrc;
 ln -nfs "${dotfiles_folder}/.gitconfig" ~/.gitconfig;

823d8b3b66d13068afcf5da5dc74afbf350c1e97
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-08-06 21:54:41 -0300

    Add more programs and extensions

--- first-boot-installs.sh
+++ first-boot-installs.sh
@@ -1,32 +1,50 @@
 mkdir ~/apps
 
-sudo add-apt-repository -y ppa:git-core/ppa  # latest git
-sudo apt-add-repository -y ppa:cappelikan/ppa  # mainline (ukuu fork)
 sudo add-apt-repository -y ppa:alessandro-strada/google-drive-ocamlfuse-beta  # https://github.com/astrada/google-drive-ocamlfuse
+sudo apt-add-repository -y ppa:cappelikan/ppa  # mainline (ukuu fork)
+sudo add-apt-repository -y ppa:git-core/ppa  # latest git
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # so we can grab the latest versions of gcc https://wiki.ubuntu.com/ToolChain
 
-sudo apt-get --install-suggests install -y \
+sudo apt-get install -y \
 	bmon \  # network speedometer
 	byobu \  # terminal application
 	cowsay \  # echo inside an ASCII cow
-	fortune \
-	google-drive-ocamlfuse git git-annex gparted \
+	dconf-editor \  # gui to edit dconf settings
+	fortune \  # print a fortune cookie sentence to stdout
+	fuse-zip \  # mount .zip files quickly https://bitbucket.org/agalanin/fuse-zip
+	gcc-10 \  # recent gnu c compiler
+	google-drive-ocamlfuse \  # mount GDrive 
+	git git-annex \
+	gparted \  # disk partitioner
 	hollywood \  # feel like you're a hollywood hacker
 	lolcat \  # rainbow-colored cat github.com/busyloop/lolcat
 	mainline \  # ubuntu kernel update utility (ukuu) replacement
 	numlockx \  # change numlock status programmatically
 	python-nautilus \  # add support for python extensions on nautilus
-	screenfetch sqlitebrowser \
+	screenfetch \
+	sqlitebrowser \  # gui to explore sqlite dbs
 	terminator \  # better terminal app
 	unrar \  # support for .rar archives
-	xclip xkbset
+	xclip \
+	xkbset
 
 
 sudo snap install --classic code
 sudo snap install --classic shotcut
 sudo snap install --classic sublime-text
+
 sudo snap install \
 	chromium code \
-	gimp htop plexmediaserver postman spotify vlc youtube-dl
+	gimp \
+	htop \
+	plexmediaserver postman \
+	spotify \
+	vlc \
+	youtube-dl
+
+
+# using multiple gcc versions https://linuxize.com/post/how-to-install-gcc-compiler-on-ubuntu-18-04/
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100
 
 
 # Change GtkIMContextSimple (emoji input) from "['<Control><Shift>e']"
@@ -48,7 +66,7 @@ pip install \
 
 
 # these two should be equivalent
-sudo apt-get install python3-gi
+# sudo apt-get install python3-gi
 pip install --user PyGObject
 
 # install nautilus-copypath extension to add "copy path" to the context menu of a file
@@ -70,11 +88,14 @@ nautilus -q
 # JDownloader installation
 bash ./installers/JD2Setup_x64.sh
 
+# Java runtime environment
+mkdir -p ~/.local/java/
+tar -xzf ./installers/jre-8u261-linux-x64.tar.gz !$
 
 # code extensions
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension mhutchie.git-graph  # https://github.com/mhutchie/vscode-git-graph
 code --install-extension ms-python.python
 code --install-extension njpwerner.autodocstring
 code --install-extension njqdev.vscode-python-typehint
-code --install-extension CoenraadS.bracket-pair-colorizer-2
-code --install-extension Tyriar.sort-lines  # github.com/Tyriar/vscode-sort-lines
-
+code --install-extension Tyriar.sort-lines  # https://github.com/Tyriar/vscode-sort-lines

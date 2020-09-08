171b787d4747d60ac1000424899be6ad5167d4e5
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2020-09-08 00:39:26 -0300

    Add transmission config file to dotfiles

--- create-dotfiles-symlinks.sh
+++ create-dotfiles-symlinks.sh
@@ -3,6 +3,7 @@ dotfiles_folder=$(realpath ./dotfiles)
 mkdir -p ~/.config/Code/User/
 
 ln -nfs "${dotfiles_folder}/.config_-_Code_-_User_-_settings.json" ~/.config/Code/User/settings.json
+ln -nfs "${dotfiles_folder}/.config_-_transmission_-_settings.json" ~/.config/transmission/settings.json
 ln -nfs "${dotfiles_folder}/.bash_aliases" ~/.bash_aliases;
 ln -nfs "${dotfiles_folder}/.bashrc" ~/.bashrc;
 ln -nfs "${dotfiles_folder}/.gitconfig" ~/.gitconfig;

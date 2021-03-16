315381ecef19449e6ad613f89c0e514f288190f0
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2021-03-16 09:15:21 -0300

    Install git from repo instead of tar-only

--- git-install.sh
+++ git-install.sh
@@ -1,20 +1,22 @@
-sudo apt-get install dh-autoreconf libcurl4-gnutls-dev libexpat1-dev \
-  gettext libz-dev libssl-dev
+# if you have git installed already:
+git clone git://git.kernel.org/pub/scm/git/git.git
+cd git
+git checkout v2.31.0
 
-sudo apt-get install asciidoc xmlto docbook2x
-
-sudo apt-get install install-info
-
+# if you don't have git installed yet
 tar -zxf git-2.31.0.tar.gz -C /usr/local/src/
 cd !$
 cd git-2.31.0
 
+
+sudo apt-get install \
+  dh-autoreconf libcurl4-gnutls-dev libexpat1-dev \
+  gettext libz-dev libssl-dev
+sudo apt-get install asciidoc xmlto docbook2x
+sudo apt-get install install-info
+
 make configure
-
 ./configure --prefix=/usr
-
 make all doc info
 
 sudo make install install-doc install-html install-info
-
-

2b1b0f28418acd65e205655cdac6c4681ae3ac9a
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2021-04-15 22:10:08 -0300

    Grab the latest docker-compose

--- docker-install.sh
+++ docker-install.sh
@@ -16,6 +16,14 @@ sudo add-apt-repository \
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# docker-compose: i know it looks hacky, but trust me lol
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod ug+x /usr/local/bin/docker-compose
+
+# so you won't need sudo all the time
+sudo groupadd docker # may already exists but it's ok
+sudo usermod -aG docker $USER
+
+# if you can't logout and log back in, use this
+newgrp docker

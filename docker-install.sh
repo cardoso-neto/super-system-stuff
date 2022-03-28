3d66cba5e84ffc179e950c830f3de2436e474ce1
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2022-03-28 12:45:36 -0300

    Add more comments to docker-compose install

--- docker-install.sh
+++ docker-install.sh
@@ -17,14 +17,16 @@ sudo add-apt-repository \
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # docker-compose: i know it looks hacky, but trust me lol
-destination=/usr/local/bin/docker-compose
+compose-bin=/usr/local/bin/docker-compose
+# grab latest tag name from Github releases
 export version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o ${destination}
-sudo chmod ug+x ${destination}
-sudo chown $USER:$USER ${destination}
+# download latest binary from Github releases
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o ${compose-bin}
+sudo chmod ug+x ${compose-bin}
+# change ownership to belong to current user and docker group
+sudo chown $USER:docker ${compose-bin}
 
 # so you won't need sudo all the time
-sudo groupadd docker # may already exists but it's ok
 sudo usermod -aG docker $USER
 
 # if you can't logout and log back in, use this

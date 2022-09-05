ac52c34192f6ed026ba2441b1bdf3fcb03f92c2e
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2022-09-05 15:04:10 -0300

    Update docker install for ubuntu 2022

--- docker-install.sh
+++ docker-install.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
 sudo apt-get install -y \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-# docker-compose: i know it looks hacky, but trust me lol
-compose-bin=/usr/local/bin/docker-compose
-# grab latest tag name from Github releases
-export version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-# download latest binary from Github releases
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o ${compose-bin}
-sudo chmod ug+x ${compose-bin}
-# change ownership to belong to current user and docker group
-sudo chown $USER:docker ${compose-bin}
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # so you won't need sudo all the time
 sudo usermod -aG docker $USER
